30478c9f3ba26ef5c40a8dd987829a8dfed06cd3
Hideo Takayan <anhlasinhvien2k51@gmail.com>
2025-05-17 13:38:04 +0700

    Update UserTest.java

---
+++ University_Management/src/test/UserTest.java
@@ -0,0 +1,16 @@
+package University_Management.src.test;
+
+import University_Management.src.model.User;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User(1, "Do_Hieu", "123456", "mhieu@gmail.com", "student");
+
+        System.out.println("=== UserTest ===");
+        System.out.println("Mã người dùng: " + user.getUserId());
+        System.out.println("Tên đăng nhập: " + user.getUsername());
+        System.out.println("Mật khẩu: " + user.getPassword());
+        System.out.println("Email: " + user.getEmail());
+        System.out.println("Vai trò: " + user.getRole());
+    }
+}
