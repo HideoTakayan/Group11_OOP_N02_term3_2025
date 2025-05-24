dd31ee3ef7d23fabc1a820759d3ae05eb26d67db
Hideo Takayan <anhlasinhvien2k51@gmail.com>
2025-05-24 15:14:29 +0700

    Update App.java

--- University_Management/src/App.java
+++ University_Management/src/App.java
@@ -1,45 +1,9 @@
 package University_Management.src;
 
+import University_Management.src.bin.;.LecturerDAO;
+import University_Management.src.bin.StudentDAO;
+import University_Management.src.bin.SubjectDAO;
+import University_Management.src.model.Lecturers;
 import University_Management.src.model.Student;
 import University_Management.src.model.Subject;
-import University_Management.src.model.Lecturers;
-// import University_Management.src.model.Users;
-import University_Management.test.UserTest;
-import University_Management.test.TimeTest;
-import University_Management.test.RecursionTest;
-
-// import University_Management.src.model.User;
-public class App {
-    public static void main(String[] args) {
-        Student student = new Student(1, "Nguyễn Văn An", "Nam", "2003-05-10");
-        System.out.println("=== StudentTest ===");
-        System.out.println("Mã sinh viên: " + student.getStudentId());
-        System.out.println("Tên sinh viên: " + student.getName());
-        System.out.println("Giới tính: " + student.getGender());
-        System.out.println("Ngày sinh: " + student.getDateOfBirth());
-
-        Subject subject = new Subject(101, "Lập trình Java", 3, 1);
-
-        System.out.println("=== SubjectTest ===");
-        System.out.println("Mã môn học: " + subject.getSubjectId());
-        System.out.println("Tên môn học: " + subject.getSubjectName());
-        System.out.println("Số tín chỉ: " + subject.getCredit());
-        System.out.println("Mã giảng viên phụ trách: " + subject.getLecturerId());
-
-        Lecturers teacher = new Lecturers(1, "Nguyễn Lệ Thu", "Nữ", "2003-05-10");
-
-        System.out.println("=== TeacherTest ===");
-        System.out.println("Mã giáo viên: " + teacher.getLecturerId());
-        System.out.println("Tên giáo viên: " + teacher.getName());
-        System.out.println("Giới tính: " + teacher.getGender());
-        System.out.println("Ngày sinh: " + teacher.getDateOfBirth());
-
-        System.out.println("=== Running TestUser ===");
-        UserTest.main(args);
-        System.out.println("\n=== Running TestTime ===");
-        TimeTest.main(args);
-        System.out.println("\n=== Running TestRecursion ===");
-        RecursionTest.main(args);
-
-    }
-}
+import University_Management.src.manager.UniversityManager;
