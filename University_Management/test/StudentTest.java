86142cd6bd0656a8c07cdb4d06a87fcf5ee371a8
d4225 <duytuantrieu123@gmail.com>
2025-05-31 07:17:31 +0000

    sửa 1.0000007

--- University_Management/test/StudentTest.java
+++ University_Management/test/StudentTest.java
@@ -1,6 +1,8 @@
 package University_Management.test;
-
+import University_Management.src.dao.GradeDAO;
 import University_Management.src.model.Student;
+import java.util.List;
+import University_Management.src.model.Grade;
 
 public class StudentTest {
     public static void main(String[] args) {
@@ -13,6 +15,14 @@ public class StudentTest {
         System.out.println("Tên: " + s1.getName());
         System.out.println("Giới tính: " + s1.getGender());
         System.out.println("Ngày sinh: " + s1.getDateOfBirth());
+        GradeDAO gradeDAO = new GradeDAO();
+        List<Grade> grades = gradeDAO.getGradesByStudentID(1);
+
+        System.out.println("\n=== Danh sách điểm của sinh viên ID 1 ===");
+        for (Grade g : grades) {
+        System.out.println("Môn: " + g.getSubjectId() + " | Điểm: " + g.getScore());
+        }
 
     }
+
 }
